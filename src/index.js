12bcbeb35ebf49221174564b2258268224ab8fc1
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-02-21 12:35:21 -0500

    create menu.js file and other changes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+//require("dotenv").config();
 const app = express();
 const port = 3000
 //response es la respuesta que le daremos al usuario
 //require es lo que recoguemos del usuario
+
+//routes (obj re peticion y obj de respuesta)
 app.get("/", (req,res)=>{
-  res.send("Hola Mundo ")
+  res.send("Welcome to my API  ")
 })
 // la puerta ya esta abierta y vemos que esta detras de la puerta
 
+//mongodb conection
+mongoose.connect(
+  "mongodb+srv://miAPI:Wn7t4RCKTXrcS4UB@cluster0.q8k7itt.mongodb.net/?retryWrites=true&w=majority")
+  .then(()=> console.log("si funciono esta conectado a mongodbatlas "))
+  .catch((error) => console.error("error"));
+//Una promesa para ver que todo salga bn con la conexion
+//y catch para tratar algun error
 
 
-//el navegadorsepa que esa es  la puerta que va a empezar a mirar
+//esto siguiente es para que el navegadorsepa que esa es  la puerta que va a empezar a mirar
 app.listen(port, ()=>{
   console.log("Mi puerto es " + port)
 })
---
+++ src/routes/menu.js
@@ -0,0 +1,14 @@
+const express =require("express");
+//este express va a tener un constructor llamado router
+const router = express.Router()
+//para crear un enrutador, que es lo que voy a estar exportando de este archivo
+
+
+//create menu
+router.post("/menu", () =>)
+
+
+
+//para luego usarlo en el servidor(las rutas se definen en archivos separados)
+module.exports = router;
+
