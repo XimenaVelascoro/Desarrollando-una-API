f12e1ce182dafa9190495b4d39baadc427938516
Laura Ximena Velasco Rocha <12ximenavelasco@gmail.com>
2023-02-23 14:54:14 -0500

    connected succesfully aweasome xD

--- config/mongo.js
+++ config/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const dbConnect = ()  => {
   // en este archivo deberia ir la conexion de la base de datos con el back
-   mongoose.connect("mongodb+srv://miAPI:Wn7t4RCKTXrcS4UB@cluster0.q8k7itt.mongodb.net/NutriLunch?retryWrites=true&w=majority")
+   mongoose.connect("mongodb+srv://API:ED6gntvJkiW43I5p@cluster0.q8k7itt.mongodb.net/NutriLunch_menu")
        .then(()=> console.log("connected to mongodb atlas"))
        .catch((error) => console.error("error"));
 //Una promesa para ver que todo salga bn con la conexion
